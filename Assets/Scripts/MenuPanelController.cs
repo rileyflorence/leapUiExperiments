667bf8cbcd03e990ddffd792a9689a1e7d9dc7f8
Riley Florence <rileyflorence@gmail.com>
2018-08-23 23:49:05 -0600

    Initial
    Here we go.

---
+++ Assets/Scripts/MenuPanelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+	}
+
+    public void translatePanel()
+    {
+        transform.Translate(Vector3.right * .5f);
+    }
+}
---
+++ Assets/Scripts/menuSounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuSounds : MonoBehaviour {
+
+    public AudioSource menuFX;
+    public AudioClip openMenu;
+    public AudioClip closeMenu;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        menuFX.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        menuFX.PlayOneShot(clickFx);
+    }
+
+    public void OpenMenuSound()
+    {
+        menuFX.PlayOneShot(openMenu);
+    }
+
+    public void CloseMenuSound()
+    {
+        menuFX.PlayOneShot(closeMenu);
+    }
+}
