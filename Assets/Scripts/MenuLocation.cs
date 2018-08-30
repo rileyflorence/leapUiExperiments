befacd703e6a571541611c8d1ab10b0400f25f75
Riley Florence <rileyflorence@gmail.com>
2018-08-29 23:59:53 -0600

    Lots of Progress
    I have it so the menu appears above your hand, but the look-at is behind the appear. Why? Also, it's backwards. I moved the controller off the menu and onto the hands.

---
+++ Assets/Scripts/MenuLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLocation : MonoBehaviour {
+
+    public Transform palmLocation;
+    public Transform menuLocation;
+    public Transform player;
+
+	public void SetMenuLocation ()
+    {
+        menuLocation.LookAt(player.position + new Vector3(0, 0.02f, 0));
+        menuLocation.position = palmLocation.position + new Vector3 (0, 0.3f, 0);
+    }
+
+}
