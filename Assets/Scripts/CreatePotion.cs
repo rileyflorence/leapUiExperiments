dabbf8160dbddb05f1c1afae3e403521b51251af
Riley Florence <rileyflorence@gmail.com>
2018-08-30 20:24:43 -0600

    Fin.
    Does what I set out to do. On to the next one!

---
+++ Assets/Scripts/CreatePotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePotion : MonoBehaviour {
+
+    public Transform potion;
+    public Transform menu;
+
+	public void MakePotion()
+    {
+        Instantiate(potion, menu.position + new Vector3(-.05f, 0.06f, -.12f), Quaternion.identity);
+    }
+}
